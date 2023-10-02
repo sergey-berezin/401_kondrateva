7984fa4772ead60ee0cd166fb359bec99eb9eab8
Alexandra Kondratyeva <a.kondratyevaa@gmail.com>
2023-10-03 00:54:17 +0300

    init commit

---
+++ app/Program.cs
@@ -0,0 +1,26 @@
+﻿using NuGetBert;
+
+namespace MyApp
+{
+    class Program
+    {
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+            string textPath = args[0];
+            var model = new Bert(textPath);
+            Console.WriteLine("session started");
+            var question = "";
+            while ((question = Console.ReadLine()) != "")
+            {
+                var answer = model.GetAnswer(question, cts.Token);
+                Console.WriteLine($"{question}: {answer.Result}");
+            }
+
+        }
+    }
+}
